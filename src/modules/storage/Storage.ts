0c21afc7c4c97e72ae5052219f2d4afd989b46de
Kang-Simon <sangyoon.kang@bosagora.io>
2022-11-29 17:28:21 +0900

    Add storage

---
+++ src/modules/storage/Storage.ts
@@ -0,0 +1,55 @@
+import mkdirp from "mkdirp";
+import path from "path";
+import sqlite3, { Database } from "sqlite3";
+import { IDatabaseConfig } from "../../service/common/Config";
+
+export class Storage {
+    protected database: Database;
+
+    constructor(config: IDatabaseConfig, callback: (err: Error | null) => void) {
+        mkdirp.sync(path.dirname(config.path));
+        this.database = new sqlite3.Database(
+            config.path,
+            sqlite3.OPEN_CREATE | sqlite3.OPEN_READWRITE,
+            (err: Error | null) => {
+                if (err) callback(err);
+                this.createTables()
+                    .then(() => {
+                        callback(null);
+                    })
+                    .catch((e) => {
+                        callback(e);
+                    });
+            }
+        );
+    }
+
+    public close(callback?: (err: Error | null) => void) {
+        this.database.close(callback);
+    }
+
+    public getDatabase(): Database {
+        return this.database;
+    }
+
+    /**
+     * Creates tables.
+     * @returns Returns the Promise. If it is finished successfully the `.then`
+     * of the returned Promise is called and if an error occurs the `.catch`
+     * is called with an error.
+     */
+    public createTables(): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            resolve();
+        });
+    }
+
+    public run(sql: string, params: any): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            this.database.run(sql, params, (err) => {
+                if (err) reject(err);
+                resolve(true);
+            });
+        });
+    }
+}
---
+++ src/service/storage/RollupStorage.ts
@@ -0,0 +1,61 @@
+import { Storage } from "../../modules/storage/Storage";
+import { IDatabaseConfig } from "../common/Config";
+import { createTableQuery, deleteQuery, dropQuery, insertQuery, selectQuery } from "./schema/RollupSchema";
+import { ITransaction } from "../types";
+
+export class RollupStorage extends Storage {
+    constructor(databaseConfig: IDatabaseConfig, callback: (err: Error | null) => void) {
+        super(databaseConfig, callback);
+    }
+
+    public createTables(): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            this.database.exec(createTableQuery, (err) => {
+                if (err) reject(err);
+                resolve();
+            });
+        });
+    }
+
+    public insert(params: ITransaction[]): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            if (params.length < 1) reject("The data is not available.");
+            const statement = this.database.prepare(insertQuery);
+            params.forEach((row) => {
+                statement.run([
+                    row.trade_id,
+                    row.user_id,
+                    row.state,
+                    row.amount.toString(),
+                    row.timestamp,
+                    row.exchange_user_id,
+                    row.exchange_id,
+                ]);
+            });
+            statement.finalize((err) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(true);
+            });
+        });
+    }
+
+    public select(trade_id: string): Promise<ITransaction[]> {
+        return new Promise<ITransaction[]>((resolve, reject) => {
+            this.database.all(selectQuery, [trade_id], (err, row) => {
+                if (err) reject(err);
+                resolve(row);
+            });
+        });
+    }
+
+    public delete(trade_id: string): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            this.database.run(deleteQuery, [trade_id], (err) => {
+                if (err) reject(err);
+                resolve(true);
+            });
+        });
+    }
+}
