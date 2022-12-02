299f73d89f4efbcf6373a1547904333a9b7a0085
HyeonyeobKim <kimhyunb@gmail.com>
2022-12-02 16:23:37 +0900

    Validate the previous block hash.
    The improved smart contract compares the previous block hash when adding.

--- test/contracts/RollUp.test.ts
+++ test/contracts/RollUp.test.ts
@@ -53,7 +53,7 @@ describe("Test of RollUp Contract", () => {
             await admin_contract.add(
                 BigNumber.from(1),
                 Utils.readFromString("0xc37e8a7e16f89b16fb6c653991b1a9ad2bb317dcee2021621e9ded79df054b2d"),
-                Utils.readFromString("0xb10edf5e825d5be4dca9a2d432ed2a997e391f70f109c4410ee0b966a83de995"),
+                Utils.readFromString("0x0000000000000000000000000000000000000000000000000000000000000000"),
                 Utils.readFromString("0xb10edf5e825d5be4dca9a2d432ed2a997e391f70f109c4410ee0b966a83de995"),
                 BigNumber.from(1669703553),
                 "QmW3CT4SHmso5dRJdsjR8GL1qmt79HkdAebCn2uNaWXF1h"
@@ -63,10 +63,23 @@ describe("Test of RollUp Contract", () => {
         const res = await admin_contract.getByHeight(BigNumber.from(1));
         assert.deepStrictEqual(res[0], BigNumber.from(1));
         assert.deepStrictEqual(res[1], "0xc37e8a7e16f89b16fb6c653991b1a9ad2bb317dcee2021621e9ded79df054b2d");
-        assert.deepStrictEqual(res[2], "0xb10edf5e825d5be4dca9a2d432ed2a997e391f70f109c4410ee0b966a83de995");
+        assert.deepStrictEqual(res[2], "0x0000000000000000000000000000000000000000000000000000000000000000");
         assert.deepStrictEqual(res[3], "0xb10edf5e825d5be4dca9a2d432ed2a997e391f70f109c4410ee0b966a83de995");
         assert.deepStrictEqual(res[4], BigNumber.from(1669703553));
         assert.deepStrictEqual(res[5], "QmW3CT4SHmso5dRJdsjR8GL1qmt79HkdAebCn2uNaWXF1h");
+
+        assert.deepStrictEqual(await admin_contract.size(), BigNumber.from(2));
+
+        await expect(
+            admin_contract.add(
+                BigNumber.from(2),
+                Utils.readFromString("0xfb70b52757351317940d8e8380142a2d9b737df3a651aa7b388a3a4186ace6d0"),
+                Utils.readFromString("0x0000000000000000000000000000000000000000000000000000000000000000"),
+                Utils.readFromString("0xb10edf5e825d5be4dca9a2d432ed2a997e391f70f109c4410ee0b966a83de995"),
+                BigNumber.from(1669704553),
+                "QmW3CT4SHmso5dRJdsjR8GL1qmt79HkdAebCn2uNaWXF1h"
+            )
+        ).to.be.revertedWith("E002: The previous block hash is not valid.");
 
         assert.deepStrictEqual(await admin_contract.size(), BigNumber.from(2));
     });
@@ -93,6 +106,6 @@ describe("Test of RollUp Contract", () => {
             admin_contract.getByHash(
                 Utils.readFromString("0x1111111111111111111111111111111111111111111111111111111111111111")
             )
-        ).to.be.revertedWith("E004: No corresponding block hash key value.");
+        ).to.be.revertedWith("E005: No corresponding block hash key value.");
     });
 });
