0d71ffdb903314d9314e1b9e304baf976d2e6070
HyeonyeobKim <kimhyunb@gmail.com>
2022-12-05 15:41:10 +0900

    Add BlocksTable and Querys

--- src/service/storage/schema/RollupSchema.ts
+++ src/service/storage/schema/RollupSchema.ts
@@ -1,8 +1,20 @@
-export const dropQuery = `
-    DROP TABLE IF EXISTS tx
+export const dropTablesQuery = `
+    DROP TABLE IF EXISTS blocks;    
+    DROP TABLE IF EXISTS tx;    
 `;
 
-export const createTxTableQuery = `
+export const createTablesQuery = `
+  CREATE TABLE IF NOT EXISTS blocks(
+    height INTEGER PRIMARY KEY,
+    cur_block TEXT,
+    prev_block TEXT,
+    merkle_root TEXT,
+    "timestamp" INTEGER,
+    CID TEXT,
+    sended INTEGER DEFAULT 0
+  );
+  CREATE INDEX curBlockHashIndex on blocks (cur_block);
+
   CREATE TABLE IF NOT EXISTS tx(
     tx_id INTEGER PRIMARY KEY AUTOINCREMENT,
     trade_id TEXT,
@@ -19,7 +31,19 @@ export const createTxTableQuery = `
   CREATE INDEX txHashIndex on tx (hash);
 `;
 
-export const insertQuery = `
+export const insertBlockQuery = `
+  INSERT INTO blocks(
+      height,
+      cur_block,
+      prev_block,
+      merkle_root,
+      "timestamp",
+      CID,
+      sended
+    ) VALUES (?,?,?,?,?,?,?)
+`;
+
+export const insertTxQuery = `
   INSERT OR REPLACE INTO tx(
     trade_id,
     user_id,
@@ -34,22 +58,34 @@ export const insertQuery = `
     ) VALUES (?,?,?,?,?,?,?,?,?,?)
 `;
 
-export const deleteByHashQuery = `
+export const selectBlockByHeightQuery = `
+    SELECT * FROM blocks WHERE height = ?
+`;
+
+export const selectBlocksLastHeight = `
+    SELECT MAX(height) as height FROM blocks
+`;
+
+export const selectBlockByHashQuery = `
+    SELECT * FROM blocks WHERE cur_block = ?
+`;
+
+export const deleteTxByHashQuery = `
     DELETE FROM tx WHERE hash = ?
 `;
 
-export const selectQueryByID = `
+export const selectTxByIDQuery = `
     SELECT * FROM tx WHERE tx_id = ?
 `;
 
-export const selectByHashQuery = `
+export const selectTxByHashQuery = `
     SELECT * FROM tx WHERE hash = ?
 `;
 
-export const selectByLengthQuery = `
+export const selectTxByLengthQuery = `
     SELECT * FROM tx ORDER BY tx_id ASC LIMIT ?
 `;
 
-export const selectLength = `
+export const selectTxsLength = `
     SELECT COUNT(tx_id) as count FROM tx
 `;
